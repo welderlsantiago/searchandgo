99799004a8759d2c316df3afec7f6afea4d41823
welderLS <welderlsantiago@gmail.com>
2023-11-20 19:47:08 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/welderlsantiago/searchandgo/router"
+import (
+	"github.com/welderlsantiago/searchandgo/config"
+	"github.com/welderlsantiago/searchandgo/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	//Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	//Initialize Router
 	router.Initialize()
 }
